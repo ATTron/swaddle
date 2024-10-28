1c31f03e0aebffa5db7b91c86f4dfd8ba0923065
Anthony Templeton <twantempleton11@gmail.com>
2024-10-28 14:16:06 -0400

    convert to logs

--- src/main.rs
+++ src/main.rs
@@ -81,11 +81,11 @@ impl IdleApp {
             match response {
                 Ok(resp) => {
                     if let Some(arg) = resp.get_items().get(0) {
-                        println!("ARG IS {:?}", arg);
+                        log::debug!("ARG IS {:?}", arg);
                         match arg {
                             MessageItem::Variant(ref value) => match **value {
                                 MessageItem::Str(ref s) => {
-                                    println!("showing unwrapped: {}", s);
+                                    log::debug!("showing unwrapped: {}", s);
                                     if s == "Playing" {
                                         self.should_block
                                             .store(true, std::sync::atomic::Ordering::SeqCst);
@@ -96,18 +96,20 @@ impl IdleApp {
                                             .store(false, std::sync::atomic::Ordering::SeqCst);
                                     }
                                 }
-                                _ => println!("Not a string variant inside Variant"),
+                                _ => log::debug!("Not a string inside the variant. . . IDK what to do so I will throw it away. It is a {:?}", value),
                             },
                             _ => {
-                                println!("Not a Variant");
+                                log::debug!(
+                                    "Not a Variant . . . IDK what to do so I will throw it away. It is a {:?}", arg
+                                );
                             }
                         }
                     } else {
-                        println!("No arguments found in the message.");
+                        log::debug!("No arguments found in the message.");
                     }
                 }
                 Err(_) => {
-                    eprintln!("Unable to lookup playback currently...skipping");
+                    log::error!("Unable to lookup playback . . . skipping");
                 }
             }
         }
@@ -157,7 +159,6 @@ impl IdleApp {
     }
 
     fn run_cmd(&mut self) -> Result<Child, Box<dyn Error>> {
-        log::debug!("command is spawning");
         match Command::new("systemd-inhibit")
             .arg("--what")
             .arg("idle")
@@ -173,13 +174,14 @@ impl IdleApp {
             .spawn()
         {
             Ok(child) => {
+                log::debug!("systemd-inhibit has been spawned");
                 self.last_block_time = Some(Instant::now());
                 self.process_running
                     .store(true, std::sync::atomic::Ordering::SeqCst);
                 Ok(child)
             }
             Err(e) => {
-                eprintln!("Failed to execute systemd-inhibit command: {:?}", e);
+                log::error!("Failed to execute systemd-inhibit command: {:?}", e);
                 Err(Box::new(std::io::Error::new(
                     std::io::ErrorKind::Other,
                     "Unable to block swayidle due to unknown error",
@@ -188,108 +190,6 @@ impl IdleApp {
         }
     }
 }
-
-// fn run(&self) -> Result<(), Box<dyn Error>> {
-//     if let Err(e) = self.dbus_runner.add_match() {
-//         eprintln!("Unable to setup dbus_runner :: {:?}", e);
-//     }
-//     let mut next_check = Instant::now();
-//     loop {
-//         let current_time = Instant::now();
-//         if current_time >= next_check {
-//             if let Ok(mut inhibit) = self.inhibit_process.lock() {
-//                 if inhibit.0.is_none() || current_time >= inhibit.1 {
-//                     let process_running = Arc::clone(&self.process_running);
-//                     if let Some(mut child) = inhibit.0.take() {
-//                         child.wait()?;
-//                         let _ = child.kill();
-//                         process_running.store(false, std::sync::atomic::Ordering::SeqCst);
-//                     }
-//                     match self
-//                         .dbus_runner
-//                         .connection
-//                         .process(Duration::from_millis(1000))
-//                     {
-//                         Ok(_) => {
-//                             let block = self
-//                                 .dbus_runner
-//                                 .good_to_send
-//                                 .load(std::sync::atomic::Ordering::SeqCst);
-//                             // Only spawn a single child if its blocking already, move on
-//                             log::debug!(
-//                                 "should_block = {} - process_running = {:?}",
-//                                 block,
-//                                 process_running,
-//                             );
-//                             if block && !process_running.load(std::sync::atomic::Ordering::SeqCst) {
-//                                 if let Some(mut killing) = inhibit.0.take() {
-//                                     killing.wait()?;
-//                                     killing.kill()?;
-//                                 }
-//                                 match self.run_cmd() {
-//                                     Ok(child) => {
-//                                         log::debug!("Swayidle is inhibiting now!");
-//                                         inhibit.0 = Some(child);
-//                                     }
-//                                     Err(e) => {
-//                                         eprintln!("unable to block swayidle :: {:?}", e)
-//                                     }
-//                                 }
-//                             } else if !block {
-//                                 if let Some(mut killing) = inhibit.0.take() {
-//                                     killing.wait()?;
-//                                     killing.kill()?;
-//                                     process_running
-//                                         .store(false, std::sync::atomic::Ordering::SeqCst);
-//                                 }
-//                             }
-//                         }
-//                         Err(e) => eprintln!("Error handling D-Bus message: {:?}", e),
-//                     }
-//                     inhibit.1 =
-//                         current_time + Duration::from_secs(INHIBIT_DURATION - OVERLAP_DURATION);
-//
-//                     next_check = current_time + Duration::from_secs(OVERLAP_DURATION);
-//                 }
-//             }
-//         }
-//         sleep(Duration::from_millis(5000));
-//     }
-// }
-
-// fn run_cmd() -> Result<Child, Box<dyn Error>> {
-//     log::debug!("command is spawning");
-//     let process_running = Arc::clone(&self.process_running);
-//     match Command::new("systemd-inhibit")
-//         .arg("--what")
-//         .arg("idle")
-//         .arg("--who")
-//         .arg("swayidle-inhibit")
-//         .arg("--why")
-//         .arg("audio playing")
-//         .arg("--mode")
-//         .arg("block")
-//         .arg("sh")
-//         .arg("-c")
-//         .arg(format!("sleep {}", self.inhibit_duration))
-//         .spawn()
-//     {
-//         Ok(child) => {
-//             if let Ok(mut last_block_time) = self.last_block_time.lock() {
-//                 *last_block_time = Some(Instant::now());
-//             }
-//             process_running.store(true, std::sync::atomic::Ordering::SeqCst);
-//             Ok(child)
-//         }
-//         Err(e) => {
-//             eprintln!("Failed to execute systemd-inhibit command: {:?}", e);
-//             Err(Box::new(std::io::Error::new(
-//                 std::io::ErrorKind::Other,
-//                 "Unable to block swayidle due to unknown error",
-//             )))
-//         }
-//     }
-// }
 
 const INHIBIT_DURATION: u64 = 25;
 
